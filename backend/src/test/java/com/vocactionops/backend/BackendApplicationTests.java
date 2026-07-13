234bc00a8585859d5eae19eb05f58f4e0e051c44
JunHo Kim <84065408+JJUNOKIM@users.noreply.github.com>
2026-07-13 14:59:18 +0900

    chore: fix testcode

--- backend/src/test/java/com/vocactionops/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/vocactionops/backend/BackendApplicationTests.java
@@ -2,10 +2,16 @@ package com.vocactionops.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:voc_actionops_test;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.show-sql=false",
+		"spring.sql.init.mode=never"
+})
 class BackendApplicationTests {
 
 	@Test
